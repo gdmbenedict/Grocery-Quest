97c36b6c339e0dfe819d0be1d72a24e2a04b898b
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-04-13 18:27:06 -0300

    Items and Inventory Added
    Added items and inventory class to the game.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public List<Item> items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool AddItem(Item.itemType type, int quantity)
+    {
+        //if adding negative quantity returns false
+        if(quantity < 0)
+        {
+            return false;
+        }
+
+        //check if item already exists
+        for (int i =0; i<items.Count; i++)
+        {
+            if (items[i].type == type)
+            {
+                items[i].quantity += quantity;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool RemoveItem(Item.itemType type, int quantity)
+    {
+        //if subtracting negative quantity returns false
+        if (quantity < 0)
+        {
+            return false;
+        }
+
+        //check if item already exists
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].type == type)
+            {
+                if (items[i].quantity - quantity < 0)
+                {
+                    return false;
+                }
+
+                items[i].quantity -= quantity;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int GetItemQuantity(Item.itemType type)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].type == type)
+            {
+                return items[i].quantity;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Inventory;
+
+public class Item : MonoBehaviour
+{
+    public enum itemType
+    {
+        gold,
+        apple,
+        banana,
+        pork,
+        oliveOil,
+        rockSalt,
+        hotSauce,
+        healthPotion,
+        manaPotion,
+        tea,
+        knife,
+        alcohol,
+        book,
+        candle,
+    }
+
+    public itemType type;
+    public int quantity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
